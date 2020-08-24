e9e5852219ade46985b3cb47b368926186c9e5f9
mateja <mateja176@gmail.com>
2020-08-24 12:39:30 +0200

    feat: completes the first part of the assignment

---
+++ readme.txt
@@ -0,0 +1,38 @@
+#    __  __                                 _ _ _   _      _
+#   |  \/  |                               | (_) | (_)    (_)
+#   | \  / | ___  _ __     __ _ _   _  __ _| |_| |_ _  ___ _  ___ _ __
+#   | |\/| |/ _ \| '_ \   / _` | | | |/ _` | | | __| |/ __| |/ _ \ '_ \
+#   | |  | | (_) | | | | | (_| | |_| | (_| | | | |_| | (__| |  __/ | | |
+#   |_|  |_|\___/|_| |_|  \__, |\__,_|\__,_|_|_|\__|_|\___|_|\___|_| |_|
+#                            | |
+#                            |_|
+
+######################################
+#  TREE TEST                         #
+######################################
+
+Welcome to the Tree Test of Mon Qualiticien.
+
+Ready ?! Go !
+
+What we would expect from you is the following :
+
+1. Use the ts library io-ts to convert a json tree (from the file tree.json) into a typed array using the interface Node described into the tree.ts file
+
+2. Once you got the array from io.ts, use it to generate a tree with the ts library fp-ts
+
+What you should get with fp-ts :
+
+CUSTOMER GROUP
+├─ BU-FRANCE
+│  ├─ BU-QUAL
+│  ├─ BU-BUSINESS
+│  └─ BU-HR
+└─ BU-GERMANY
+   ├─ BU-QUAL
+   ├─ BU-BUSINESS
+   └─ BU-HR
+
+Good Luck ;)
+
+
---
+++ tree.ts
@@ -0,0 +1,50 @@
+import { isRight } from 'fp-ts/lib/These';
+import * as io from 'io-ts';
+import treeJSON from './tree.json';
+
+interface Node {
+  id: number;
+  name: string;
+  children: number[];
+  parent: number | null;
+}
+
+type NodeArray = Node[];
+
+const NodeType = io.type({
+  id: io.number,
+  name: io.string,
+  children: io.array(io.number),
+  parent: io.union([io.null, io.number]),
+});
+
+const NodeArrayType = io.array(NodeType);
+
+const nodes = NodeArrayType.decode(treeJSON);
+
+interface Tree {
+  [key: string]: {} | Tree;
+}
+
+const fold = (nodes: NodeArray, children: Node['children']): Tree =>
+  children.reduce((tree, nodeId) => {
+    const node = nodes.find(({ id }) => id === nodeId);
+
+    if (node) {
+      return { ...tree, [node.name]: fold(nodes, node.children) };
+    } else {
+      throw new Error(`Cannot find node by id "${nodeId}"`);
+    }
+  }, {} as Tree);
+
+if (isRight(nodes)) {
+  const root = nodes.right.find((node) => node.parent === null);
+
+  if (root) {
+    console.log(JSON.stringify(fold(nodes.right, [root.id]), null, 2));
+  } else {
+    console.error('No root node');
+  }
+} else {
+  console.error('Invalid JSON.');
+}
