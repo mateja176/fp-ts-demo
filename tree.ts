a0cd9e89a2014515e58f23d7cc0c0521c4aba349
mateja <mateja176@gmail.com>
2020-08-24 15:21:06 +0200

    feat: curries reduce

--- tree.ts
+++ tree.ts
@@ -24,24 +24,29 @@ const nodes = NodeArrayType.decode(treeJSON);
 
 type NameTree = fp.tree.Tree<Node['name'] | Error>;
 
-const reduce = (
-  nodes: NodeArray,
+const reduce = (nodes: NodeArray) => (
   children: Node['children'],
 ): fp.tree.Forest<Node['name'] | Error> =>
-  fp.array.map((id: Node['id']) => {
-    const node = fp.array.findFirst((node: Node) => node.id === id)(nodes);
+  fp.function.pipe(
+    children,
+    fp.array.map((id: Node['id']) => {
+      const node = fp.array.findFirst((node: Node) => node.id === id)(nodes);
 
-    if (fp.option.isSome(node)) {
-      return fp.tree.make(node.value.name, reduce(nodes, node.value.children));
-    } else {
-      return fp.tree.tree.of(new Error(`Cannot find node by id "${id}"`));
-    }
-  })(children);
+      if (fp.option.isSome(node)) {
+        return fp.tree.make(
+          node.value.name,
+          reduce(nodes)(node.value.children),
+        );
+      } else {
+        return fp.tree.tree.of(new Error(`Cannot find node by id "${id}"`));
+      }
+    }),
+  );
 
 const mapNodesToTree = (nodes: NodeArray) => (root: Node) => {
   const nameTree: NameTree = fp.tree.make(
     root.name,
-    reduce(nodes, root.children),
+    reduce(nodes)(root.children),
   );
 
   return nameTree;
